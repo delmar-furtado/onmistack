0046841dbfa83721f2d81b42571c2d563cb5e850
Delmar <delmar@green4.com.br>
2020-04-30 11:15:02 -0300

    frontend do app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,13 @@
-import React, {useState} from 'react';
-import Header from './Header';
+import React from 'react';
+import './global.css';
+import Routes from './routes';
 
 //JSX (JavaScrip XML)
 
 function App() {
-  const [counter, setCounter] = useState(0);
-  // Array[valor, funcaoAtualizacao]
-
-  function increment() {
-    setCounter(counter + 1);
-  }
-  return (
-    <div>
-      <Header>Contador: {counter}</Header>
-      <button onClick={increment}>Incrementar</button>
-    </div>
-  );
+    return (
+        <Routes />
+    );
 }
 
 export default App;
